73f4adcb46dd326402878d4085017df8e8f31bac
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-11-28 12:58:31 -0500

    zinggrid

--- api/views.py
+++ api/views.py
@@ -4,7 +4,10 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Max, F
 from django.db import connection
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import BugSerializer
 
 
