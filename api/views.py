82c7cc366244ffec1d6b28766811b11e643e1ea7
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-11-28 13:28:58 -0500

    zinggrid

--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from serializers import UserSerializer, GroupSerializer
+from .serializers import UserSerializer, GroupSerializer
 
 
 ##################################### API/JSON Views #############################
