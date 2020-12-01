9f02e93a6607d4e2014ed26b8acafa1c55ab3561
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-12-01 00:07:37 -0500

    zinggrid

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register(r'buglisting', views.BugsSerializer)
+#router.register(r'buglist', views.BugsSerializer)
 
 urlpatterns = [
     path('', include(router.urls)),
