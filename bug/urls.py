f7b6b6a7cb5a693803ce7f71acc45b8eb885e416
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-12-01 00:21:57 -0500

    zinggrid

--- bug/urls.py
+++ bug/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 #import from import views
 from bug import views
 urlpatterns = [
-	path('bug/', views.bug_list_view, name='bug-list'),
+	path('', views.bug_list_view, name='bug-list'),
     path('create/', views.bug_create_view , name='bug-create' ),
     path('create/success', views.bug_create_view , name='bug-create-success' ),
     path('<int:id>/', views.bug_detail_view, name='bug-detail'),
