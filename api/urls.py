7ab9c20df686443e1d30b03887d9903c202168ad
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-12-01 19:20:29 -0500

    zinggrid

--- api/urls.py
+++ api/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
 
     path('workqueue-all/', views.json_bug_list_all, name='wq-all'),
     path('workqueue-filtered/', views.json_bug_list_wq, name='wq-filtered'),
-    #path('bug-list/', views.BugsSerializer, name='bug-list'),
 ] 
