c0fa42b29ac62d0d8f277e447f88a2a509ae214e
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-12-02 21:34:19 -0500

    zinggrid

--- BugTracker/settings/production.py
+++ BugTracker/settings/production.py
@@ -183,10 +183,13 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Allow Messages to be passed
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
