efd2768f5997540923d38a4ce9e4360d11a7bf94
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-12-23 18:47:21 -0500

    zinggrid

--- bug/views.py
+++ bug/views.py
@@ -1130,7 +1130,7 @@ def zing_cal_requests(request):
                             "borderColor": "#454754"
                         }
                     },
-                    "palette": ["#00ace6", "#454754"],
+                    "palette": ["#00ace6", "#0097A7"],
                     "rows": 2,
                     "scale": {
                         "width": "30%",
