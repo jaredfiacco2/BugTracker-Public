f1722b7c6eb70a29310f93ed09e47ed8ee7921da
jaredfiacco2 <jaredfiacco2@gmail.com>
2020-12-24 08:56:14 -0500

    zinggrid

--- BugTracker/settings/production.py
+++ BugTracker/settings/production.py
@@ -38,7 +38,7 @@ SECURE_FRAME_DENY               = True
 SECRET_KEY = 'f5d642eb-8fe9-4501-8686-84010bf18d27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bugresolve-pm.herokuapp.com', 'bugtrackertools.com', 'www.bugtrackertools.com', 'localhost', '127.0.0.1']
 
